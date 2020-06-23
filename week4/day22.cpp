ae43396cb2ef55bef2e703b5c08c4134b2236d77
suryansh <suryanshsharma132@gmail.com>
2020-06-23 09:48:34 +0530

    week4

--- week4/day22.cpp
+++ week4/day22.cpp
@@ -16,6 +16,10 @@ Example 2:
 Input: [0,1,0,1,0,1,99]
 Output: 99
 */
+
+//Solution 1 : Using sort+linear traversal;
+//TC: O(nlogn+N) ; SC:O(1)  
+//Since it is given N<=INT_MAX so logn<= 32
 auto i1 = []()
 {
         ios_base::sync_with_stdio(false);
@@ -54,5 +58,59 @@ public:
             return ans;
             
         
+    }
+};
+
+//Solution 2: Using Bit manipulation
+//TC:O(n) and SC:O(1)
+/*
+basically the approach is to have two variables ones and twos(initially zero)
+one by one do this
+ones = (ones XOR a[i])&(complement(twos))
+twos = (twos XOR a[i])&(complement(ones))
+*/
+
+auto i1 = []()
+{
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        return 0;
+} ();
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+            int ones = 0;
+            int twos = 0;
+            for(auto n:nums)
+            {
+                    ones = (ones^n)&(~twos);
+                    twos = (twos^n)&(~ones);
+            }
+            return ones;
+    }
+};
+
+
+//Solution 3: Using set bits;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+           int n=nums.size();
+        unsigned int ans=0,shift=1;
+        for(int i=0;i<32;++i)
+        {
+            int count=0;
+            for(auto ele: nums)
+            {
+                if(ele & shift)
+                    count+=1;
+            }
+            if(count%3 != 0)
+                ans += shift;
+            shift*=2;
+            
+        }
+        return ans;
     }
 };
